f738f4881369bef054f1c82275cb1dea4bf5296a
Ashley Smith <ashesmsmith@gmail.com>
2025-01-22 10:20:00 -0700

    Added logos, colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "dark-green": "535E1C",
+        "light-green": "9BB07E",
+        "dark-brown": "543A27",
+        "light-brown": "8A5D3D",
+        "cream": "F1ECE2",
       },
+      fontFamily: {
+        sans: 'Montserrat',
+        serif: 'Cormorant',
+      }
     },
   },
   plugins: [],
