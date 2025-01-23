ed8aa3925f6f6bc53f451ac1479386728b41ff48
Ashley Smith <ashesmsmith@gmail.com>
2025-01-23 13:28:00 -0700

    submit for review/database not yet seeded

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -39,82 +39,61 @@ async function seedProducts() {
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         productName VARCHAR (255) NOT NULL,
         productDesc TEXT NOT NULL,
-        amount INT NOT NULL,
+        price INT NOT NULL,
         imageSRC TEXT NOT NULL
         );
     `;
 
-    const insertedInvoices = await Promise.all(
-        invoices.map(
-        (invoice) => client.sql`
-            INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+    const insertedProducts = await Promise.all(
+        products.map(
+        (product) => client.sql`
+            INSERT INTO products (id, productName, productDesc, price, imageSRC)
+            VALUES (${product.id}, ${product.productName}, ${product.productDesc}, ${product.price}, ${product.imageSRC})
             ON CONFLICT (id) DO NOTHING;
         `,
         ),
     );
 
-    return insertedInvoices;
+    return insertedProducts;
 }
 
-async function seedCustomers() {
+async function seedReviews() {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
     await client.sql`
-        CREATE TABLE IF NOT EXISTS customers (
+        CREATE TABLE IF NOT EXISTS reviews (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        image_url VARCHAR(255) NOT NULL
+        prodId NOT NULL,
+        customerName VARCHAR(255) NOT NULL,
+        stars INT NOT NULL,
+        date VARCHAR(255) NOT NULL
         );
     `;
 
-    const insertedCustomers = await Promise.all(
-        customers.map(
-        (customer) => client.sql`
-            INSERT INTO customers (id, name, email, image_url)
-            VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+    const insertedReviews = await Promise.all(
+        reviews.map(
+        (review) => client.sql`
+            INSERT INTO customers (id, prodId, customerName, stars, review, date)
+            VALUES (${review.id}, ${review.prodId}, ${review.customerName}, ${review.stars}, ${review.review}, ${review.date})
             ON CONFLICT (id) DO NOTHING;
         `,
         ),
     );
 
-    return insertedCustomers;
-}
-
-async function seedRevenue() {
-    await client.sql`
-        CREATE TABLE IF NOT EXISTS revenue (
-        month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
-        );
-    `;
-
-    const insertedRevenue = await Promise.all(
-        revenue.map(
-        (rev) => client.sql`
-            INSERT INTO revenue (month, revenue)
-            VALUES (${rev.month}, ${rev.revenue})
-            ON CONFLICT (month) DO NOTHING;
-        `,
-        ),
-    );
-
-    return insertedRevenue;
+    return insertedReviews;
 }
 
 export async function GET() {
-        try {
+    try {
         await client.sql`BEGIN`;
-        await seedUsers();
-        await seedCustomers();
-        await seedInvoices();
-        await seedRevenue();
+        await seedSellers();
+        await seedProducts();
+        await seedReviews();
         await client.sql`COMMIT`;
-    
+
         return Response.json({ message: 'Database seeded successfully' });
-        } catch (error) {
+    } catch (error) {
         await client.sql`ROLLBACK`;
         return Response.json({ error }, { status: 500 });
-        }
+    }
 }
