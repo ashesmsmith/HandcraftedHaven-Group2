f076026d36d9fa9333f18e37265eba27d12e96c6
Ashley Smith <ashesmsmith@gmail.com>
2025-02-15 09:43:30 -0700

    resolve conflict

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -27,6 +27,7 @@ export async function fetchProductById(product_id: string) {
     try {
         const data = await sql<ProductsTable>`
             SELECT * FROM products 
+            JOIN reviews ON products.product_id = reviews.product.id
             WHERE product_id = ${product_id}
         `;
 
