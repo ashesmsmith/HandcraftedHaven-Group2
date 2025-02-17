52b4efa2baefac08c7be8942dbf7a8eaeab71a23
kellyannclark <103697379+kellyannclark@users.noreply.github.com>
2025-02-17 16:11:18 -0700

    file adjusted

--- next.config.ts
+++ next.config.ts
@@ -6,22 +6,8 @@ const nextConfig = {
     POSTGRES_URL: process.env.POSTGRES_URL,
     DATABASE_URL: process.env.DATABASE_URL,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.giftedartisan.com", // ✅ Existing domain
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com", // ✅ Google images
-      },
-      {
-        protocol: "https",
-        hostname: "www.kuiu.com", // ✅ Added new domain for Kuiu images
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
+
+
